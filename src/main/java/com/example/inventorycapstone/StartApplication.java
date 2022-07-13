e8b393334ae54672c000672b53d00e727f4c5f5e
Coen-Sarah <sarah.e.coen.07@gmail.com>
2022-07-13 11:02:10 -0500

    Rough completion of Login, Main Page, fxml GUI.

--- src/main/java/com/example/inventorycapstone/StartApplication.java
+++ src/main/java/com/example/inventorycapstone/StartApplication.java
@@ -7,26 +7,28 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+import static com.example.inventorycapstone.doa.DBConnection.makeConnection;
 import static javafx.application.Platform.exit;
 
 public class StartApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 700, 400);
+        FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource("main_menu.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Miniature Managment!");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
-        /*try{
+
+        try{
+            launch();
             makeConnection();
 
         }catch(Exception e){
             e.printStackTrace();
-        }*/
+        }
 
         exit();
     }
--- src/main/java/com/example/inventorycapstone/controller/MainController.java
+++ src/main/java/com/example/inventorycapstone/controller/MainController.java
@@ -7,6 +7,8 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Accordion;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TitledPane;
 import javafx.stage.Stage;
 
@@ -18,11 +20,135 @@ public class MainController {
     public Accordion classStockReport;
     public ArrayList<TitledPane> reportItems = new ArrayList<TitledPane>();
 
+    public Button miniAddNew;
+    public MenuItem officialSetAddNew;
+    public MenuItem customSetAddNew;
+    public Button classAddNew;
+    public Button miniChange;
+    public Button miniDiscard;
+    public Button setChange;
+    public Button setDiscard;
+    public Button classChange;
+    public Button classDiscard;
+    private String[] changeText = {"Edit", "Save"};
+    private String[] discardText = {"Delete", "Cancel"};
+
+
+
     public MainController() {
     }
 
     public void initialize(){
         addClassReportContent();
+        //initializeButtons();
+    }
+
+    private void initializeButtons() {
+        
+        miniAddNew.setOnAction(event ->{
+            if(miniChange.getText().equals(changeText[0])){
+                //TODO set blank details form editable
+                miniChange.setText(changeText[1]);
+                miniDiscard.setText(discardText[1]);
+            }else{
+                //TODO cancel form and
+            }
+        });
+        miniChange.setOnAction(event -> {
+            if(miniChange.getText().equals(changeText[0])){
+                //TODO set details form editable
+                miniChange.setText(changeText[1]);
+                miniDiscard.setText(discardText[1]);
+            }else{
+                //TODO save form
+                miniChange.setText(changeText[0]);
+                miniDiscard.setText(discardText[0]);
+            }
+        });
+        miniDiscard.setOnAction(event -> {
+            if(miniDiscard.getText().equals(discardText[0])){
+                //TODO delete item
+                miniChange.setText(changeText[1]);
+                miniDiscard.setText(discardText[1]);
+            }else{
+                //TODO cancel saving details form
+                miniChange.setText(changeText[0]);
+                miniDiscard.setText(discardText[0]);
+            }
+        });
+
+        officialSetAddNew.setOnAction(event ->{
+            if(setChange.getText().equals(changeText[0])){
+                //TODO set blank details form editable
+                setChange.setText(changeText[1]);
+                setDiscard.setText(discardText[1]);
+            }else{
+                //TODO cancel form and
+            }
+        });
+        customSetAddNew.setOnAction(event ->{
+            if(setChange.getText().equals(changeText[0])){
+                //TODO set blank details form editable
+                setChange.setText(changeText[1]);
+                setDiscard.setText(discardText[1]);
+            }else{
+                //TODO cancel form and
+            }
+        });
+        setChange.setOnAction(event -> {
+            if(setChange.getText().equals(changeText[0])){
+                //TODO set details form editable
+                setChange.setText(changeText[1]);
+                setDiscard.setText(discardText[1]);
+            }else{
+                //TODO save form
+                setChange.setText(changeText[0]);
+                setDiscard.setText(discardText[0]);
+            }
+        });
+        setDiscard.setOnAction(event -> {
+            if(setDiscard.getText().equals(discardText[0])){
+                //TODO delete item
+                setChange.setText(changeText[1]);
+                setDiscard.setText(discardText[1]);
+            }else{
+                //TODO cancel saving details form
+                setChange.setText(changeText[0]);
+                setDiscard.setText(discardText[0]);
+            }
+        });
+
+        classAddNew.setOnAction(event ->{
+            if(classChange.getText().equals(changeText[0])){
+                //TODO set blank details form editable
+                classChange.setText(changeText[1]);
+                classDiscard.setText(discardText[1]);
+            }else{
+                //TODO cancel form and
+            }
+        });
+        classChange.setOnAction(event -> {
+        if(classChange.getText().equals(changeText[0])){
+            //TODO set details form editable
+            classChange.setText(changeText[1]);
+            classDiscard.setText(discardText[1]);
+        }else{
+            //TODO save form
+            classChange.setText(changeText[0]);
+            classDiscard.setText(discardText[0]);
+        }});
+        classDiscard.setOnAction(event -> {
+            if(classDiscard.getText().equals(discardText[0])){
+                //TODO delete item
+                classChange.setText(changeText[1]);
+                classDiscard.setText(discardText[1]);
+            }else{
+                //TODO cancel saving details form
+                classChange.setText(changeText[0]);
+                classDiscard.setText(discardText[0]);
+            }
+        });
+
     }
 
     private void addClassReportContent() {
@@ -39,7 +165,22 @@ public class MainController {
         }
     }
 
-    @FXML
+    private void addMiniDetailsContent(){
+        //TODO CREATE MINI EXTERNAL FXML
+        //TODO ADD CONTENT TO MINI DETAILS PANE
+    }
+
+    private void addSetDetailsContent(){
+        //TODO CREATE OFFICIAL SET EXTERNAL FXML
+        //TODO CREATE CUSTOM SET EXTERNAL FXML
+        //TODO ADD CONTENT TO SET DETAILS PANE
+    }
+
+    private void addClassDetailsContent(){
+        //TODO CREATE CLASS VIEW EXTERNAL FXML
+        //TODO CREATE CLASS EDIT EXTERNAL FXML
+        //TODO ADD CONTENT TO CLASS DETAILS PANE
+    }
     public void logout(ActionEvent event){
         try{
             toLogin(event);
